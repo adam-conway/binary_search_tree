78ea07641bc84dcd261d217e703990397395a46c
adam-conway <adam.n.conway@gmail.com>
2018-01-30 08:42:29 -0700

    Progress on insertion method

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -2,16 +2,37 @@ require 'pry'
 require './lib/node.rb'
 
 class BinarySearchTree
-  attr_reader :root_node, :node
+  attr_reader :root, :depth, :current_node
 
   def initialize
-    @root_node = nil
+    @root = nil
+    @current_node = nil
+    @depth = 0
   end
 
   def insert(score, title)
-    @node = Node.new(score, title)
-    if @root_node == nil
-      @root_node = @node
+    if @root == nil
+      @root = Node.new(score, title)
+      @current_node = root
+      depth
+    else
+      positional_node(score, title, current_node)
+    end
+  end
+
+  def positional_node(score, title, current_node)
+    if score < current_node.movie_details.keys[0]
+      if current_node.left_node == nil
+        current_node.left_node = Node.new(score, title)
+      else
+        positional_node(score, title, current_node.left_node)
+      end
+    else
+      if current_node.right_node == nil
+        current_node.right_node = Node.new(score, title)
+      else
+        positional_node(score, title, current_node.right_node)
+      end
     end
   end
 
--- test/binary_search_tree_test.rb
+++ test/binary_search_tree_test.rb
@@ -5,16 +5,33 @@ require './lib/binary_search_tree.rb'
 
 class BinarySearchTreeTest < Minitest::Test
 
+  def setup
+
+  end
+
   def test_new_binary_search_tree_creation
     tree = BinarySearchTree.new
     assert_instance_of BinarySearchTree, tree
-    assert_nil tree.root_node
+    assert_nil tree.root
+    assert_equal tree.depth, 0
   end
 
-  def test_inserting_nodes_in_tree
+  def test_insert_setting_root_node
     tree = BinarySearchTree.new
     tree.insert(61, "Bill & Ted's Excellent Adventure")
-    assert_instance_of Node, tree.node
-    assert tree.root_node, tree.node
+    assert_instance_of Node, tree.root
+    assert_equal tree.root, tree.current_node
+  end
+
+  def test_inserting_depth_1
+    tree = BinarySearchTree.new
+    tree.insert(61, "Bill & Ted's")
+    assert_equal tree.root, tree.current_node
+    assert_equal tree.root.movie_details.keys[0], 61
+    assert_equal tree.root.movie_details.values[0], "Bill & Ted's"
+    tree.insert(16, "Johnny English")
+    assert_equal tree.root.left_node.movie_details.keys[0], 16
+    tree.insert(92, "Sharknado 3")
+    assert "It's higher!"
   end
 end
